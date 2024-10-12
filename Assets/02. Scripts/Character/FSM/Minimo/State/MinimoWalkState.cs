d3db08a8d184b07a0010d6a9f94ad0394c352bfe
dah <daun792@gmail.com>
2024-10-12 22:03:58 +0900

    [PathFinding] [Mod] A* 알고리즘으로 변경..

--- Assets/02. Scripts/Character/FSM/Minimo/State/MinimoWalkState.cs
+++ Assets/02. Scripts/Character/FSM/Minimo/State/MinimoWalkState.cs
@@ -1,21 +1,35 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class MinimoWalkState : StateBase<Minimo>
 {
-    private const float RADIUS = 10;
+    private const float SPEED = 0.3f;
 
-    private readonly PathFinder _path;
+    private bool _isUpdating = false;
+    private PathManager _pathManager;
+    private List<Vector3Int> _path;
+    private Vector3 _targetPosition;
+
+    private int _currIndex;
 
     public MinimoWalkState(Minimo owner) : base(owner) 
     {
-        _path = owner.GetComponent<PathFinder>();
+        _pathManager = App.Instance.GetManager<PathManager>();
     }
 
     public override void Enter()
     {
-        if (_path.SetTarget(GetRandomPosition(), OnEndPathFinding))
+        _path = _pathManager.GetRandomPath(_owner.transform.position);
+
+        if (_path != null && _path.Count > 0)
         {
             _owner.SetAnimation("Walk");
+
+            _currIndex = 0;
+            _targetPosition = _pathManager.GetTileWorldPosition(_path[_currIndex]);
+
+            _isUpdating = true;
         }
         else
         {
@@ -23,25 +37,39 @@ public class MinimoWalkState : StateBase<Minimo>
         }
     }
 
-    public override void Execute() { }
+    public override void Execute()
+    {
+        if (!_isUpdating)
+        {
+            return;
+        }
+
+        if (_currIndex < _path.Count)
+        {
+            if ((_targetPosition - _owner.transform.position).sqrMagnitude > 0.01f)
+            {
+                _owner.transform.position = Vector3.MoveTowards(_owner.transform.position, _targetPosition, SPEED * Time.deltaTime);
+            }
+            else
+            {
+                if (++_currIndex < _path.Count)
+                {
+                    _targetPosition = _pathManager.GetTileWorldPosition(_path[_currIndex]);
+                }
+            }
+        }
+        else
+        {
+            _isUpdating = false;
+            _owner.SetChillState();
+        }
+    }
 
     public override void Exit()
     {
-        _path.StopPathFinding();
-    }
+        _isUpdating = false;
+        _path = null;
 
-    private Vector3 GetRandomPosition()
-    {
-        var center = _owner.transform.position;
-
-        Vector2 randomCircle = Random.insideUnitCircle * RADIUS;
-        Vector3 randomPosition = new(center.x + randomCircle.x, center.y + randomCircle.y, center.z);
-
-        return randomPosition;
-    }
-
-    private void OnEndPathFinding()
-    {
-        _owner.SetChillState();
+        _currIndex = 0;
     }
 }
