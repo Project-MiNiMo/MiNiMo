d7f258ec5cc71773dd9b2721f529f8cc3a196e01
HaramK <37320983+HaramK@users.noreply.github.com>
2024-10-16 00:37:04 +0900

    [추가] 빌드 옵션(iOS/AOS)

--- Assets/02. Scripts/Build/Editor/BuildScript.cs
+++ Assets/02. Scripts/Build/Editor/BuildScript.cs
@@ -9,11 +9,25 @@ class BuildScript
     static string[] SCENES = FindEnabledEditorScenes();
 
     [MenuItem("CustomBuild/Perform Build")]
-    static void PerformBuild()
+    static void PerformBuildWindow()
     {
         // build for mac
         BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Test", BuildTarget.StandaloneOSX, BuildOptions.None);
     }
+    
+    [MenuItem("CustomBuild/Perform Build iOS")]
+    statiic void PerformBuildiOS()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Test", BuildTarget.iOS, BuildOptions.None);
+    }
+    
+    [MenuItem("CustomBuild/Perform Build Android")]
+    static void PerformBuildAndroid()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Test", BuildTarget.Android, BuildOptions.None);
+    }
+    
+    
     private static string[] FindEnabledEditorScenes()
     {
         List<string> EditorScenes = new List<string>();
