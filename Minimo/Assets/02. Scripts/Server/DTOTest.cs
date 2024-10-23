d135f64fb865ddae640fcaf56fc383fc2ecaabc3
HaramK <37320983+HaramK@users.noreply.github.com>
2024-10-24 02:21:51 +0900

    [추가] MinimoSharead dll

---
+++ Minimo/Assets/02. Scripts/Server/DTOTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MinimoShared;
+
+public class DTOTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var playerDTO = new PlayerDTO();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
