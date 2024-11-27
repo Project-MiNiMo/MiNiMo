2a6cd24ed6c0df1c6054684fdc95eb3f7be885dc
HaramK <37320983+HaramK@users.noreply.github.com>
2024-11-27 22:34:09 +0900

    [추가][로그인] 특정 ManagerBase 덮어씌워지지 않도록 Init 씬으로 분리

---
+++ Minimo/Assets/02. Scripts/Test/LoadLoginScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLoginScene : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("96. Login");
+    }
+}
