1ff48a388d9de9183130e8e3592c33cfd2e9eea0
HaramK <37320983+HaramK@users.noreply.github.com>
2024-12-04 23:26:14 +0900

    [추가] 건물용 DTO 추가

---
+++ Minimo/Assets/02. Scripts/Server/Shared/Account/BuildingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace MinimoShared
+{
+    public class BuildingDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsInstalled { get; set; }
+        public Vector3 Position { get; set; }
+    }
+}
