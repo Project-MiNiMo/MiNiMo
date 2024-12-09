96f3e1cc1724d7cb72fe3f77553825ce1f0d8a20
dah <daun792@gmail.com>
2024-12-09 22:59:32 +0900

    [Mod] [Produce] ProduceOptionBtn에 결과물 이름 표시

--- Minimo/Assets/02. Scripts/UI/Produce/ProduceOptionBtn.cs
+++ Minimo/Assets/02. Scripts/UI/Produce/ProduceOptionBtn.cs
@@ -13,16 +13,19 @@ public class ProduceOptionBtn : MonoBehaviour
         public TextMeshProUGUI _text;
     }
     
+    [SerializeField] private TextMeshProUGUI _resultNameTMP;
     [SerializeField] private ProduceOptionInfo _result;
     [SerializeField] private ProduceOptionInfo[] _materials;
     [SerializeField] private TextMeshProUGUI _timeTMP;
     
     private ProduceManager _produceManager;
+    private TitleData _titleData;
     private int currentIndex;
 
     private void Start()
     {
         _produceManager = App.GetManager<ProduceManager>();
+        _titleData = App.GetData<TitleData>();
         
         GetComponent<Button>().onClick.AddListener(StartProduce);
     }
@@ -56,6 +59,13 @@ public class ProduceOptionBtn : MonoBehaviour
 
     private void SetResultInfo(ProduceResult result)
     {
+        if (!_titleData.Item.TryGetValue(result.Code, out var itemData))
+        {
+            Debug.LogError($"Cannot find item data with code : {result.Code}");
+            return;
+        }
+        
+        _resultNameTMP.text = _titleData.GetString(itemData.Name);
         _result._image.sprite = Resources.Load<Sprite>($"Item/{result.Code}");
         _result._text.text = $"X{result.Amount}";
     }
