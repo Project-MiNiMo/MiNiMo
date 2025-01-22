a8eb6d22cdb957adc75b1141a03feb5ff73332d9
dah <daun792@gmail.com>
2025-01-22 18:54:14 +0900

    [Mod] CycleSystem 서버 시간과 연동

--- Minimo/Assets/02. Scripts/GameSystem/CycleSystem.cs
+++ Minimo/Assets/02. Scripts/GameSystem/CycleSystem.cs
@@ -3,7 +3,6 @@ using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-// TODO : 추후 서버 시간으로 대체
 public class CycleSystem : MonoBehaviour
 {
     [Serializable]
@@ -17,43 +16,48 @@ public class CycleSystem : MonoBehaviour
     [SerializeField] private CycleMark[] _marks;
     [SerializeField] private Light2D _globalLight;
 
+    private TimeManager _timeManager;
+    
     private const float CYCLE_LENGTH = 24f;
-    private float _lastUpdateTime = 0f;
+    private DateTime _lastUpdateTime;
     private float _time;
 
     private int _currentIndex = 0;
 
     private void Start()
     {
+        _timeManager = App.GetManager<TimeManager>();
+        
         Array.Sort(_marks, (a, b) => a.timeRatio.CompareTo(b.timeRatio));
 
+        _lastUpdateTime = _timeManager.Time;
+        
         UpdateTime();
         FindCurrentIndex();
-        UpdateLight(0);
+        UpdateLight(CalculateTransitionFactor());
     }
 
     private void Update()
     {
-        if (Time.time - _lastUpdateTime >= 60f)
-        {
-            _lastUpdateTime = Time.time;
+        if ((_timeManager.Time - _lastUpdateTime).TotalSeconds < 1f) return;
+        
+        _lastUpdateTime = _timeManager.Time;
 
-            UpdateTime();
-            FindCurrentIndex();
-            UpdateLight(CalculateTransitionFactor());
-        }
+        UpdateTime();
+        FindCurrentIndex();
+        UpdateLight(CalculateTransitionFactor());
     }
 
     private void UpdateTime()
     {
-        DateTime now = DateTime.Now;
+        var now = _timeManager.Time;
 
         _time = (now.Hour + now.Minute / 60f) / CYCLE_LENGTH;
     }
 
     private void FindCurrentIndex()
     {
-        for (int i = 0; i < _marks.Length; i++)
+        for (var i = 0; i < _marks.Length; i++)
         {
             if (_time < _marks[i].timeRatio)
             {
@@ -67,31 +71,31 @@ public class CycleSystem : MonoBehaviour
 
     private float CalculateTransitionFactor()
     {
-        CycleMark currentMark = _marks[_currentIndex];
-        CycleMark nextMark = _marks[(_currentIndex + 1) % _marks.Length];
+        var currentMark = _marks[_currentIndex];
+        var nextMark = _marks[(_currentIndex + 1) % _marks.Length];
 
-        float currentTime = currentMark.timeRatio;
-        float nextTime = nextMark.timeRatio;
+        var currentTime = currentMark.timeRatio;
+        var nextTime = nextMark.timeRatio;
 
         if (nextTime < currentTime)
         {
             nextTime += 1f;
         }
 
-        float timeSinceCurrent = _time - currentTime;
+        var timeSinceCurrent = _time - currentTime;
         if (timeSinceCurrent < 0)
         {
             timeSinceCurrent += 1f;
         }
 
-        float duration = nextTime - currentTime;
+        var duration = nextTime - currentTime;
         return Mathf.Clamp01(timeSinceCurrent / duration);
     }
 
     private void UpdateLight(float updateTime)
     {
-        CycleMark currentMark = _marks[_currentIndex];
-        CycleMark nextMark = _marks[(_currentIndex + 1) % _marks.Length];
+        var currentMark = _marks[_currentIndex];
+        var nextMark = _marks[(_currentIndex + 1) % _marks.Length];
 
         _globalLight.color = Color.Lerp(currentMark.color, nextMark.color, updateTime);
         _globalLight.intensity = Mathf.Lerp(currentMark.intensity, nextMark.intensity, updateTime);
