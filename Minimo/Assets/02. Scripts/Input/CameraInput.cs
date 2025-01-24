5a27935010751e8ad7947a5c2d25e527308f1049
dah <daun792@gmail.com>
2025-01-24 20:50:00 +0900

    [Fix] 카메라 제한 시 게임이 멈추던 문제 수정

--- Minimo/Assets/02. Scripts/Input/CameraInput.cs
+++ Minimo/Assets/02. Scripts/Input/CameraInput.cs
@@ -45,6 +45,7 @@ public class CameraInput : MonoBehaviour
         var delta = new Vector3(-Input.GetAxis("Mouse X") * _dragSpeed, -Input.GetAxis("Mouse Y") * _dragSpeed, 0);
         _mainCamera.transform.Translate(delta * Time.deltaTime, Space.World);
         
+        ClampCameraPosition();
         _editCirclePanel.SetPosition();
     }
 
@@ -61,7 +62,8 @@ public class CameraInput : MonoBehaviour
             var deltaDistance = currentDistance - prevDistance;
 
             _mainCamera.orthographicSize = Mathf.Clamp(_mainCamera.orthographicSize - deltaDistance * _zoomSpeed * Time.deltaTime, _minZoom, _maxZoom);
-
+            
+            ClampCameraPosition();
             _editCirclePanel.SetPosition();
         }
         
@@ -69,8 +71,26 @@ public class CameraInput : MonoBehaviour
         if (scroll != 0.0f)
         {
             _mainCamera.orthographicSize = Mathf.Clamp(_mainCamera.orthographicSize - scroll * _zoomSpeed, _minZoom, _maxZoom);
-
+            
+            ClampCameraPosition();
             _editCirclePanel.SetPosition();
         }
+    }
+    
+    private void ClampCameraPosition()
+    {
+        var cameraHeight = _mainCamera.orthographicSize;
+        var cameraWidth = _mainCamera.orthographicSize * _mainCamera.aspect;
+        
+        if (_maxBounds.x - _minBounds.x < cameraWidth * 2 || _maxBounds.y - _minBounds.y < cameraHeight * 2)
+        {
+            Debug.LogWarning("Bounds 크기가 카메라 크기보다 작습니다. ClampCameraPosition을 건너뜁니다.");
+            return;
+        }
+        
+        var clampedX = Mathf.Clamp(_mainCamera.transform.position.x, _minBounds.x + cameraWidth, _maxBounds.x - cameraWidth);
+        var clampedY = Mathf.Clamp(_mainCamera.transform.position.y, _minBounds.y + cameraHeight, _maxBounds.y - cameraHeight);
+
+        _mainCamera.transform.position = new Vector3(clampedX, clampedY, _mainCamera.transform.position.z);
     }
 }
