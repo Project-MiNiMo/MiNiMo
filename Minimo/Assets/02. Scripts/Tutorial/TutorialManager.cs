d78921b434b835e83f5f73ec1c7cf04705d05ba2
dah <daun792@gmail.com>
2025-01-25 10:52:53 +0900

    [Add] 마지막 연출 추가...........

--- Minimo/Assets/02. Scripts/Tutorial/TutorialManager.cs
+++ Minimo/Assets/02. Scripts/Tutorial/TutorialManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 public class TutorialManager : ManagerBase
 {
     [SerializeField] private List<TutorialBase> _tutorials;
+    [SerializeField] private Transform[] _circleTransforms;
+    [SerializeField] private Transform _endTransform;
+    [SerializeField] private Image _lastImg;
+    [SerializeField] private TextMeshProUGUI _wishText1;
+    [SerializeField] private TextMeshProUGUI _wishText2;
 
     public string Wish;
 
@@ -31,12 +39,33 @@ public class TutorialManager : ManagerBase
         }
         else
         {
-            EndTutorial();
+            StartCoroutine(EndTutorial());
         }
     }
 
-    private void EndTutorial()
+    private IEnumerator EndTutorial()
     {
+        _wishText1.text = $"{Wish}....";
+        foreach (var circle in _circleTransforms)
+        {
+            circle.DOMove(_endTransform.position, 1f);
+
+            yield return new WaitForSeconds(0.3f);
+        }
         
+        var sequence = DOTween.Sequence();
+        sequence.Append(_lastImg.DOFade(1, 1f))
+            .AppendInterval(1f)
+            .Append(_wishText1.DOFade(1, 1f))
+            .Append(_wishText1.DOFade(1, 1))
+            .AppendInterval(2f)
+            .OnComplete(() =>
+            {
+                App.GetManager<UIManager>().FadeIn(() =>
+                {
+                    App.GetManager<LoginManager>().Logout();
+                    App.LoadScene(SceneName.Title);
+                });
+            });
     }
 }
